a7c8a01887e7ccf07a3dac0e5cf054799336bfff
Gagan Singh <141204441+GaganHonor@users.noreply.github.com>
2023-08-08 10:18:29 +0530

    Update top_languages.py

--- top_languages.py
+++ top_languages.py
@@ -31,11 +31,11 @@ def get_titles(query):
         # Print an error message if the response status code is not 200 (OK)
         print(f"Error: {response.status_code}")
 
-# Define a function that takes a list of titles as an input and returns a list of programming languages based on the words in the titles
+# Define a function that takes a list of titles as an input and returns a list of web languages based on the words in the titles
 def get_languages(titles):
-    # Create an empty list to store the languages
+    # Create an empty list to store the web languages
     language_list = []
-    # Define a set of common words that are not programming languages
+    # Define a set of common words that are not web languages
     common_words = {"the", "of", "in", "to", "for", "and", "a", "an", "is", "are", "be", "by", "on", "with", "as", "or", "at", "from", "how", "what", "why", "when", "where", "who"}
     # Loop through each title in the titles list
     for title in titles:
@@ -52,13 +52,28 @@ def get_languages(titles):
     # Return the language list
     return language_list
 
-# Define a function that takes a list of languages as an input and returns a list of tuples with the language name and frequency sorted in descending order
+# Define a function that takes a list of web languages as an input and returns a list of tuples with the web language name and frequency sorted in descending order
 def get_top_languages(languages):
-    # Create a Counter object from the languages list that counts the frequency of each language
+    # Create a Counter object from the languages list that counts the frequency of each web language
     counter = Counter(languages)
-    # Get a list of tuples with the language name and frequency sorted by frequency in descending order
+    # Get a list of tuples with the web language name and frequency sorted by frequency in descending order
     top_languages = counter.most_common()
     # Return the top languages list
     return top_languages
 
-# Define a query for my web search tool (you can change this according to your
+# Define a query for my web search tool (you can change this according to your interest or preference)
+query = "top web languages"
+
+# Call the get_titles function with the query as an argument and store the result in a variable called titles
+titles = get_titles(query)
+
+# Call the get_languages function with the titles as an argument and store the result in a variable called languages
+languages = get_languages(titles)
+
+# Call the get_top_languages function with the languages as an argument and store the result in a variable called top_languages
+top_languages = get_top_languages(languages)
+
+# Print the top 5 web languages based on the titles of the articles from my web search tool
+print(f"The top 5 web languages based on the titles of the articles from my web search tool are:")
+for i in range(5):
+    print(f"{i+1}. {top_languages[i][0]} ({top_languages[i][1]} times)")
